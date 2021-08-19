b7a8f88e2d4e6ca6bdbfc5e5be86709415c98b54
yujin9619 <yujin9588@gmail.com>
2021-08-19 15:15:02 +0900

    다른 화면 으로 이동

--- app/src/main/java/com/nepplus/intetn_0819/MainActivity.kt
+++ app/src/main/java/com/nepplus/intetn_0819/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.nepplus.intetn_0819
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToOtherBtn.setOnClickListener {
+
+//            버튼이 눌리면 other activity로 이동
+
+//            출발지 : main activity -> 도착지 : other activity
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+
+
+        }
+
+
     }
 }
