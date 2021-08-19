629971c7b95d08de82f124edca1faf5aa6a35b07
yujin9619 <yujin9588@gmail.com>
2021-08-19 15:34:06 +0900

    이전 화면 으로 복귀

--- app/src/main/java/com/nepplus/intetn_0819/OtherActivity.kt
+++ app/src/main/java/com/nepplus/intetn_0819/OtherActivity.kt
@@ -1,11 +1,26 @@
 package com.nepplus.intetn_0819
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        moveToMainBtn.setOnClickListener {
+
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+//            이전화면 복귀 : 현재 화면을 종료.
+
+            finish()
+
+        }
+
+
     }
 }
